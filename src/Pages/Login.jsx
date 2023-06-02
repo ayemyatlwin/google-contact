d64cf2025922b30ee8e2f620e9b38b1419bb9239
Hannah <erikahn05@gmail.com>
2023-06-02 12:42:18 +0630

    updated 2

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -24,38 +24,70 @@ const Login = () => {
  }
     
   return (
-    <div className=''>
-     
-      <div className='p-6 items-center bg-gray-100 shadow-lg h-screen'>
-      <form className='w-96 flex flex-col   mx-auto  gap-6 shadow-lg p-7' onSubmit={loginHandler}   action="">
-      <div className=' gap-2 flex justify-center items-center pb-[50px]'>
-        <RiLoginBoxFill className='text-2xl text-secondary'/>
-        <p className='font-bold text-secondary  text-2xl '>Login</p>
-        </div>
-        <hr className='border border-primary ' />
-        <div >
-        
-        <div className='flex flex-col gap-4 '>
-        <input value={email} onChange={e=> setEmail(e.target.value)} className='input  border-primary px-12 ' type="text" name="" id="" placeholder='Your Emial Address' />
-        <input value={password} onChange={e=> setPassword(e.target.value)} className='input  border-primary px-12 '  type="text" name="" id="" placeholder='Enter Your Password' />
-        
-        </div>
-        <a
-        className="inline-block align-baseline font-bold text-sm text-black mt-3 hover:text-blue-800"
-        href="#"
-      >
-        Forgot Password?
-      </a>
-        <div className='flex gap-3 p-6 '>
-            <button className='  px-12 register rounded py-2 text-white bg-secondary hover:bg-primary'>Login</button>
-            <Link to={'/register'}>
+    <div className="">
+      <div className="flex p-6 items-center bg-gray-100 shadow-lg h-screen justify-center">
+        <form
+          className="w-96 flex flex-col   mx-auto  gap-6 shadow-lg p-7"
+          onSubmit={loginHandler}
+          action=""
+        >
+          <div className=" gap-2 flex justify-center items-center pb-[50px]">
+            <RiLoginBoxFill className="text-2xl text-secondary" />
+            <p className="font-bold text-secondary  text-2xl ">Login</p>
+          </div>
+          <hr className="border border-primary " />
+          <div>
+            <div className="flex flex-col gap-4 ">
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input  border-primary px-12 "
+                type="text"
+                name=""
+                id=""
+                placeholder="Your Email Address"
+              />
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="input border-primary px-12 "
+                type="password"
+                name=""
+                id=""
+                placeholder="Enter Your Password"
+              />
+            </div>
+            {/* <a
+              className="inline-block align-baseline font-bold text-sm text-black mt-3 hover:text-blue-800"
+              href="#"
+            >
+              Forgot Password?
+            </a> */}
+            <div className="flex flex-wrap justify-center items-center gap-2 mt-3">
+              <p className="text-gray-700 font-medium">
+                Don't have an account?
+              </p>
+              <Link to={"/register"}>
+                <p className="text-blue-600 font-medium cursor-pointer">
+                  Register
+                </p>
+              </Link>
+            </div>
+            
+              <Link to={'/'}>
+              
+              <button className="px-12 register rounded py-2 text-white bg-secondary hover:bg-primary mx-auto block mt-5">
+                Login
+              </button>
+              </Link>
+              {/* <Link to={'/register'}>
             <button className='px-12 py-2 login rounded text-white bg-red-500 hover:bg-red-700'>Logout</button>
-            </Link>
-        </div>
-        </div>
-      </form>
+            </Link> */}
+            
+          </div>
+        </form>
       </div>
-      </div>
+    </div>
       
   )
 }
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -27,38 +27,79 @@ const Register = () => {
 
   }
   return (
-    <div className=''>
-      <div className='p-6 justify-end bg-gray-100 shadow-lg '>
-      <form className='w-96 flex flex-col h-screen mx-auto gap-6 shadow-lg p-7' onSubmit={registerHandler} >
-      <div className='flex items-center justify-center gap-2 pb-[50px]'>
-        <FaRegistered className='text-2xl text-secondary'/>
-        <p className='font-bold text-secondary text-2xl tracking-wider '>Register</p>
+    <div className="">
+    <div className=" flex h-screen justify-center items-center p-6 bg-gray-100 shadow-lg ">
+      <form
+        className="w-96 flex flex-col mx-auto gap-6 shadow-lg p-7"
+        onSubmit={registerHandler}
+      >
+        <div className="flex items-center justify-center gap-2 pb-[50px]">
+          <FaRegistered className="text-2xl text-secondary" />
+          <p className="font-bold text-secondary text-2xl tracking-wider ">
+            Register
+          </p>
         </div>
-        <hr className='border border-primary ' />
-        <div className='flex flex-col gap-4'>
-        <input value={name} onChange={e=> setName(e.target.value)} className='input  border-primary px-12 '  type="text" name="" id="" placeholder='Your Name' />  
-        <input value={email} onChange={e=> setEmail(e.target.value)} className='input  border-primary px-12 ' type="text" name="" id="" placeholder='Your Email Address' />
-        <input value={password} onChange={e=> setPassword(e.target.value)} className='input  border-primary px-12 '  type="text" name="" id="" placeholder='Enter Your Password' />    
-        <input value={password_confirmation} onChange={e=> setPasswordConfirmation(e.target.value)} className='input  border-primary px-12 '  type="text" name="" id="" placeholder='Password Confirmation' />
-        <div className='flex gap-2'>
-          <p>Already have an account?</p>
-          <Link to={'/login'}>
-          <p className='text-blue-500'>Login</p>
-          </Link>
-        </div>
-        <div className='flex gap-3 p-6 '>
-            <button className='  px-12 register py-2 rounded text-white bg-secondary hover:bg-primary'>Register</button>
-            <Link to={'/login'}>
-            <button className='px-12 py-2 login rounded text-white bg-primary hover:bg-secondary-focus'>Login</button>
+        <hr className="border border-primary " />
+        <div className="flex flex-col gap-4">
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="input  border-primary px-12 "
+            type="text"
+            name=""
+            id=""
+            placeholder="Your Name"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="input  border-primary px-12 "
+            type="text"
+            name=""
+            id=""
+            placeholder="Your Email Address"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input  border-primary px-12 "
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Your Password"
+          />
+          <input
+            value={password_confirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+            className="input  border-primary px-12 "
+            type="text"
+            name=""
+            id=""
+            placeholder="Password Confirmation"
+          />
+          <div className="flex gap-2 mx-auto">
+            <p>Already have an account?</p>
+            <Link to={"/login"}>
+              <p className="text-blue-500">Login</p>
             </Link>
-        </div>
+          </div>
+          <div className="flex gap-3 p-6 ">
+            <button className=" mx-auto px-12 register py-1 rounded text-white bg-secondary hover:bg-primary">
+              Register
+            </button>
+            {/* <Link to={"/login"}>
+              <button className="px-12 py-2 login rounded text-white bg-primary hover:bg-secondary-focus">
+                Login
+              </button>
+            </Link> */}
+          </div>
         </div>
       </form>
-      </div>
-      </div>
+    </div>
+  </div>
       
-    
-  )
-}
+);
+};
+
 
 export default Register
